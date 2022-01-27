47de796aed19fe9f91f0ab3a76519998376f7a89
Philip Crotwell <crotwell@seis.sc.edu>
2022-01-27 14:49:09 -0500

    lint on the lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "plugins": [
-        "standard",
-        "promise"
-    ],
-    "env": {
-      "es6": true,
-      "browser": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "sourceType": "module",
-    },
-    "rules": {
-      "semi": ["error", "always"],
-      "no-unused-vars": "off",
-      "no-console": "off"
-    }
-};
+  extends: 'eslint:recommended',
+  plugins: [
+    'standard',
+    'promise'
+  ],
+  env: {
+    es6: true,
+    browser: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  rules: {
+    semi: ['error', 'always'],
+    'no-unused-vars': 'off',
+    'no-console': 'off'
+  }
+}
